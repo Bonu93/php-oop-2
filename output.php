c6ea1476fb7599dc96de5eab286bd621303a6025
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-10 17:16:23 +0100

    complete

--- output.php
+++ output.php
@@ -16,7 +16,13 @@ $product1 = new Product('iPhone12', 900, 'Apple');
 $product2 = new Product('S21', 780, 'Samsung');
 $product3 = new Product('Galaxy Fold', 1200, 'Samsung');
 
-echo "Utente {$user2->name}, hai diritto a uno sconto del {$user2->getDiscount()}%, puoi acquistare {$product1->name} a {$product1->getSale($user2->getDiscount())}€";
+echo "Utente {$user2->name}, hai diritto a uno sconto del {$user2->getDiscount()}%, puoi acquistare {$product1->name} a {$product1->getSale($user2->getDiscount())}€ <br><br>";
+
+echo "Utente {$user3->name}, hai diritto a uno sconto del {$user3->getDiscount()}%, puoi acquistare {$product3->name} a {$product1->getSale($user3->getDiscount())}€ <br><br>";
+
+echo "Utente {$user2->name}, hai diritto a uno sconto del {$user2->getDiscount()}%, puoi acquistare {$product2->name} a {$product2->getSale($user2->getDiscount())}€ <br><br>";
+
+echo "Utente {$user1->name}, non hai diritto a nessuno sconto, puoi acquistare {$product1->name} a {$product1->price}€ <br>";
 
 
 ?>
